b7e7718500e9ee9616613d01fd92e0071db5e842
Sash Catanzarite <sashcatanzarite@gmail.com>
2019-03-25 22:25:23 -0700

    cms edits

--- src/js/cms-preview-templates/passes.js
+++ src/js/cms-preview-templates/passes.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Feature = ({feature}) =>
+const Feature = ({name}) =>
     <li class ="is-full column pass-feature">
-        {feature}
+        {name}
     </li>
 
 const Features = ({data}) => data && data.length > 0
     ? <ul class="is-10 columns is-multiline is-mobile bullet-list">
-      {data.map(({feature}) => <Feature feature={feature}/>)}
+      {data.map(({name}) => <Feature name={name}/>)}
     </ul>
     : "";
 
@@ -23,16 +23,19 @@ export default class PostPreview extends React.Component {
                 <div class="is-10-mobile is-offset-1-mobile is-offset-1-tablet is-5-tablet column pass-info">
                         <h2 class="title  is-2 is-uppercase">{entry.getIn(["data", "heading"])}</h2>
                         <h3 class="title is-4">{entry.getIn(["data", "subhead"])}</h3>
-                        <Features/>
-                        <h2 class="title  is-4 need-code">{entry.getIn(["data", "apply_headline"])}</h2>
-                        <p>{entry.getIn(["data", "apply_text"])}</p>
-                        <a class="button is-white is-medium" href="/apply">{entry.getIn(["data", "apply_button"])}</a>
+                        <ul class="is-10 columns is-multiline is-mobile bullet-list">
+                          <Features/>
+                        </ul>
                 </div>
-                <div class="is-12-mobile is-4-tablet is-4-desktop column is-offset-1-desktop">
+                <div class="is-10-mobile is-offset-1-mobile is-4-tablet is-4-desktop column is-offset-1-desktop">
+                    <h4 class="title  is-6 need-code">{entry.getIn(["data", "apply_headline"])}</h4>
+                          <p><small>{entry.getIn(["data", "apply_text"])}</small></p>
+                          <a class="button is-white" href="/apply">{entry.getIn(["data", "apply_button"])}</a>
                 </div>
       </div>
     </div>
-    </div>
-  </section>;
+  </div>
+  </section>
+  ;
   }
 }
