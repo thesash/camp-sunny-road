be84716c462fe61e2f833878496d2d1bc120d08a
Sash Catanzarite <sashcatanzarite@gmail.com>
2019-03-25 22:54:28 -0700

    activities

--- src/js/cms-preview-templates/activities.js
+++ src/js/cms-preview-templates/activities.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+const Activity = ({name}) =>
+  <li class="title is-6-mobile is-4-tablet is-3-desktop column activities-list">
+        {name}
+    </li>
+
+const Features = ({data}) => data && data.length > 0
+    ? <ul class="is-10 columns is-multiline is-mobile bullet-list">
+      {data.map(({name}) => <Activity name={name}/>)}
+    </ul>
+    : "";
+
 export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
-    let image = getAsset(entry.getIn(["data", "image"]));
+    const entryActivities = entry.getIn(["data", "activities"]);
+    const activities = entryActivities ? entryActivities.toJS() : [];
 
-    return ;
+    return <section id="activities" class="hero is-info is-fullheight activities">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns">
+                        <div class="is-10-mobile is-offset-1-mobile is-10-tablet is-offset-1-tablet column">
+                            <h2 class="is-2 is-uppercase title">{entry.getIn(["data","heading"])}</h2>
+                            <Activities data={activities}/>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>;
   }
 }
