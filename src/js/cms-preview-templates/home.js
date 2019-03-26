1cf3876b829cea9b7217ceb64ceee2d1ef7aa0d7
Sash Catanzarite <sashcatanzarite@gmail.com>
2019-03-25 22:14:53 -0700

    you kidding me, netlify? react previews just DONT WORK LOCALLY

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -6,6 +6,17 @@ export default class PostPreview extends React.Component {
     let image = getAsset(entry.getIn(["data", "image"]));
 
 
-    return <img src="/logo.png"/> ;
+    return <section class="hero is-link map-image is-fullheight home-cta has-navbar-fixed-top">   
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title is-1 uppercase">{entry.getIn(["data", "heading"])}</h1>
+            <h2 class="title is-4">{entry.getIn(["data", "subhead"])}</h2>
+            <h3 class="subtitle">{entry.getIn(["data", "subtitle"])}</h3>
+                <a href="{{.url}}" class="button is-large is-primary">
+                    <strong>{entry.getIn(["data", "button"])}</strong>
+                </a>
+            </div>
+          </div>
+    </section>;
   }
 }
