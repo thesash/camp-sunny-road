da24657d2a2a2df5da54eedaf7326a847be81aa6
Sash Catanzarite <sashcatanzarite@gmail.com>
2019-03-26 00:18:19 -0700

    Revert "delete unused files"
    This reverts commit a482e4a3b0c8237cf890784cd3aa6d70951be168.

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,6 +3,8 @@ import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
+import ProductsPreview from "./cms-preview-templates/products";
+import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 import PassesPreview from "./cms-preview-templates/passes";
 import LocationPreview from "./cms-preview-templates/location";
@@ -27,6 +29,8 @@ CMS.registerPreviewStyle("/css/main.css");
 CMS.registerPreviewStyle("https://use.fontawesome.com/releases/v5.8.1/css/all.css");
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("products", ProductsPreview);
+CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.registerPreviewTemplate("passes", PassesPreview);
 CMS.registerPreviewTemplate("location", LocationPreview);
